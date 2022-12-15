3f0ddd68c68c9f621ecb67c366e9b40954e453b6
Lovegiver <lovegiver@proton.me>
2022-12-15 02:20:14 +0100

    Commentaires

--- src/main/java/com/talan/kata/cor/classes/DelegatedDispenser.java
+++ src/main/java/com/talan/kata/cor/classes/DelegatedDispenser.java
@@ -1,16 +1,24 @@
 package com.talan.kata.cor.classes;
 
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Pour limiter la duplication du code, on centralise le code commun dans une classe unique.<br>
+ * Cette implémentation de l'interface {@link MoneyDispenser} accepte un argument supplémentaire dans
+ * son constructeur. Son utilisation n'est pas obligatoire, elle ne fait que proposer un traitement par défaut.
+ */
 class DelegatedDispenser implements MoneyDispenser {
     private final MoneyDispenser dispenser;
     private final int buckValue;
 
-    public DelegatedDispenser(MoneyDispenser dispenser, int buckValue) {
+    public DelegatedDispenser(@NotNull MoneyDispenser dispenser, int buckValue) {
         this.dispenser = dispenser;
         this.buckValue = buckValue;
     }
 
     /**
-     * @param amount
+     * Traitement par défaut.
+     * @param amount Montant restant à servir
      */
     @Override
     public void dispenseBucks(int amount) {
